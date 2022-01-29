d6b43390f8e3ff794ae2bb8fc0497be44ddfd9f9
ksenialazer <kseniya.lazarenko.27@mail.ru>
2022-01-29 21:15:18 +0500

    первый коммит

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+     byte b = -128; // [-128, 127] 1 байт
+        short s = -32768; // [-32768; 32767] 2 байта
+        int i = 2_147_483_647; //[-2147483648, 2147483647] 4 байта
+        long l = 9223372036854775807L; //[-922372036854775808,922372036854775807] 8 байт
+        float f = 3.4028235E38f; //4 байта
+        double d = 1.7976931348623158E308;//8 байт
+        char a = 'a'; // 2 байта
+        boolean e = true; // 4 байта
+
 	// write your code here
     }
 }
